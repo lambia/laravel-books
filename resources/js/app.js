9e389a73aa8b5038fd212122dff62ef23782971d
Luca Lambiase <luca.lambia@gmail.com>
2024-01-23 11:54:27 +0100

    minor

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,7 @@ import './bootstrap'; //non è la libreria Bootstrap CSS
 
 import '~resources/scss/app.scss'
 
-import * as bootstrap from 'bootstrap'
+import * as bootstrap from 'bootstrap' //Bootstrap CSS
 
 import.meta.glob([
     '../img/**'
